73617a61fa273245fef70d4a392ca7a3309a5dfd
bink3060 <bink3060@gmail.com>
2019-11-16 11:47:24 +0100

    section 8 - course project directe done

--- firstpro-angular-complete-couse/src/app/shared/dropdown.directive.ts
+++ firstpro-angular-complete-couse/src/app/shared/dropdown.directive.ts
@@ -10,4 +10,9 @@ export class DropdownDirective {
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
+/* closing dropdown from anywhere
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}*/
 }
