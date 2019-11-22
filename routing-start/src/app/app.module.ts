b9fd4f58cbdc2ac7c61b444bd9be77c8437ae409
bink3060 <bink3060@gmail.com>
2019-11-22 10:50:50 +0100

    section 11: Routing 143

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,6 +11,21 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent , children: [
+      { path: ':id/:name', component: UserComponent }
+    ]},
+  { path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ] },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found'} // always put at the end
+];
 
 @NgModule({
   declarations: [
@@ -20,11 +35,13 @@ import { ServersService } from './servers/servers.service';
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
