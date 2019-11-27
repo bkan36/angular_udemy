0b114dad069b53b62363261667a2e775f16bc8f8
bink3060 <bink3060@gmail.com>
2019-11-27 09:59:14 +0100

    section 11: Routing done

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -15,11 +15,14 @@ export class ServerComponent implements OnInit {
   }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
+    this.route.data.subscribe((data) => {
+      this.server = data['server'];
+    });
+/*    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params['id']);
-    });
+    });*/
   }
 
   onEdit() {
